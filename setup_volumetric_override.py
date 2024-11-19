0dd01e16d32ff251cba1e87379e65c35e4e4a73e
masatake1115 <masatake1115@hotmail.co.jp>
2024-11-19 12:12:24 +0900

    hgdfhgfdhgdf

---
+++ setup_volumetric_override.py
@@ -0,0 +1,20 @@
+﻿
+#============ Setup boundingbox node ==============#
+#hou.node("..").createNode("box")
+
+
+#========= Setup building generator utility node ======#
+# Create node
+n = hou.node("..").createNode("labs::building_generator_utility::2.0")
+# Select "VolumetricVerride" radio button
+n.parm("folder01").set(3)
+# Set module name
+n.parm("string1").set("!!!!!!!")
+
+
+#============= TODO: Connect boundingbox to utility node input[0] ==========#
+
+
+
+#============= Debug snippets =================#
+#print( n.asCode() )# Commadn to check radio buttion parameter name
